b2233485831fc09c63b51d6c2f476ef88078b306
Amina-Dirie <adirie522@gmail.com>
2023-06-12 07:36:37 -0700

    feat: Organizing the files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { position, turnLeft,turnRight,moveForward } from "./rover";
+import { position, turnLeft,turnRight,moveForward } from "./SpinningControl";
 
 export function nextMove(grid:number[], currentPosition:position, commands:string[]): position | string{
     let newPosition = currentPosition;
+    let isInvalid= false;
     commands.forEach(command => {
         if (command === "L") {
             newPosition.direction = turnLeft(newPosition.direction);
@@ -12,14 +13,26 @@ export function nextMove(grid:number[], currentPosition:position, commands:strin
         else if (command === "M") {
             newPosition = moveForward(newPosition);
         }
+        else{
+            isInvalid = true;
+        }
     });
-    if (grid[0] < newPosition.X || grid[1] < newPosition.Y ) {
-        return "not enough space"
+    
+    if (!isInvalid) {
+        if (grid[0] < newPosition.X || grid[1] < newPosition.Y ) {
+            return "not enough space"
+        }
+        else{
+             return newPosition;
+        }
+        
     }
     else{
-         return newPosition;
+        return "invalid command"
+
     }
-      
+
+  
    
 
 }
--- src/rover.test.ts
+++ src/rover.test.ts
@@ -27,5 +27,16 @@ describe("The rover", ()=>{
         expect(nextMove([5,5],currentPosition,commands)).toEqual("not enough space")
     })
 
+    test('returns invalid command when an invalid command has been passed', () => {
+        let currentPosition = {
+            "X": 0,
+            "Y":0,
+            "direction": 'N' 
+        } 
+        // lands in 6,6 S which is outside the grid
+        let commands = ["K", "M", "M", "M", "K", "M","R","M", "M"]
+        expect(nextMove([5,5],currentPosition,commands)).toEqual(`invalid command`)
+    })
+
 });
 
