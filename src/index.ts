62b161c3314c8329dc1e90d84e299bad22b79362
Amina-Dirie <adirie522@gmail.com>
2023-06-14 10:22:48 -0700

    feat: Adding user input

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
 import { position, turnLeft,turnRight,moveForward } from "./SpinningControl";
+import * as readline from 'readline';
+// adding a user input
+const ask = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+ask.question('What are the commands? ', (commands: string) => {
+    let theCurrentPosition = {
+        "X": 0,
+        "Y":0,
+        "direction": 'N' 
+    }
+    let splited = commands.split('');
+
+    let result = nextMove([5,5],theCurrentPosition,splited);
+    console.log(result);
+  
+  ask.close();
+});
+
 
 export function nextMove(grid:number[], currentPosition:position, commands:string[]): position | string{
     let newPosition = currentPosition;
